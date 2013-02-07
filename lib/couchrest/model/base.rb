8bc354462219ffd58d301272d3a9f33b0eb014dc
Viacheslav Petrenko <slavap@gmail.com>
2013-02-07 13:28:38 +0200

    Allow doc type to be redefined from attrinbutes on new object initialization.

--- lib/couchrest/model/base.rb
+++ lib/couchrest/model/base.rb
@@ -54,7 +54,7 @@ module CouchRest
         # set the instance's database, if provided
         self.database = options[:database] unless options[:database].nil?
         unless self['_id'] && self['_rev']
-          self[self.model_type_key] = model_type_value
+          self[self.model_type_key] = model_type_value if self['type'].blank?
         end
 
         yield self if block_given?
